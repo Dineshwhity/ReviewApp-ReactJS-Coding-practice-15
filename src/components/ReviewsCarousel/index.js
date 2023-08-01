df5ab6fe6d7dfdcbf51851080d44fa3202b74c54
T.DINESHKUMAR <137904123+Dineshwhity@users.noreply.github.com>
2023-07-31 22:53:56 -0700

    Update index.js

--- src/components/ReviewsCarousel/index.js
+++ src/components/ReviewsCarousel/index.js
@@ -55,7 +55,7 @@ class ReviewsCarousel extends Component {
           <button
             type="button"
             onClick={this.onClickLeftArrow}
-            testid="leftArrow"
+            data-testid="leftArrow"
             className="arrow-button"
           >
             <img
@@ -67,7 +67,7 @@ class ReviewsCarousel extends Component {
           <button
             type="button"
             onClick={this.onClickRightArrow}
-            testid="rightArrow"
+            data-testid="rightArrow"
             className="arrow-button"
           >
             <img
